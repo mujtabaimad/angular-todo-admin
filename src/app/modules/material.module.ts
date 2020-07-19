cc5e91dc50370893f5dc25ec3c9895a1440f9353
mujtabaimad <mujtabaimad8@gmail.com>
2020-07-19 17:49:44 +0400

    created material.module.ts to handel material design library related imports

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,10 +1,38 @@
-import { NgModule } from '@angular/core';
-import {MatSliderModule} from '@angular/material/slider';
-
+import {NgModule} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
-  imports: [MatSliderModule],
-  exports: [MatSliderModule],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {
+}
