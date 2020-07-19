982d5189f1308e2b65187173965c113f0a416f78
mujtabaimad <mujtabaimad8@gmail.com>
2020-07-19 18:03:09 +0400

    created the logic for connecting the components with the ngrx store and handling the user interactions with the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {getfilteredTodosList, getTodosList, State} from './app.reducer';
+import {AppService} from './app.service';
+import {Todo} from './todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'assessment';
+export class AppComponent implements OnInit {
+
+  constructor(private store: Store<State>, private appService: AppService) {
+  }
+
+  allTodos: Todo[];
+
+  onSearch(event): void {
+    this.appService.filterTodos(event.target.value);
+    this.allTodos = this.allTodos.filter((todo) => todo.title.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()));
+  }
+
+  onDelete(todoId): void {
+    this.allTodos = this.allTodos.filter((todo) => todo.id !== todoId);
+  }
+
+  ngOnInit(): void {
+    this.appService.fetchTodos();
+    this.store.select(getfilteredTodosList).subscribe((todos) => {
+      this.allTodos = todos;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
+import {AppComponent} from './app.component';
+import {StoreModule} from '@ngrx/store';
+import {appReducer} from './app.reducer';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {MaterialModule} from './modules/material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {TodoCardComponent} from './todo-card/todo-card.component';
+import {FormsModule} from '@angular/forms';
+import {AppService} from './app.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoCardComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers
-    }),
+    StoreModule.forRoot({appTodos: appReducer}),
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
