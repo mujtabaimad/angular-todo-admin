abdd30c77410fea932752f1c603dbe7e60dc94d2
mujtabaimad <mujtabaimad8@gmail.com>
2020-07-22 06:22:50 +0400

    for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyARgDObxaS7ZwBwjBn9CeSUv2JncXpOfVI',
+    authDomain: 'mujtabatodoapp.firebaseapp.com',
+    databaseURL: 'https://mujtabatodoapp.firebaseio.com',
+    projectId: 'mujtabatodoapp',
+    storageBucket: 'mujtabatodoapp.appspot.com',
+    messagingSenderId: '48727204072',
+    appId: '1:48727204072:web:47189081c2fb613f68295d',
+    measurementId: 'G-T5FSL988FM'
+  }
 };
