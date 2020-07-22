9e22ae02a47d154d99f1c2598f20848137777a66
mujtabaimad <mujtabaimad8@gmail.com>
2020-07-22 06:14:42 +0400

    created login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  errorMessage: string;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email]
+      }),
+      password: new FormControl('', {validators: [Validators.required]})
+    });
+  }
+
+  onSubmit(): void {
+    this.authService.login({
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password
+    }, (error) => {
+      this.errorMessage = error;
+    });
+  }
+
+  // ngOnDestroy() {
+  //   if (this.loadingSubs) {
+  //     this.loadingSubs.unsubscribe();
+  //   }
+  // }
+}
